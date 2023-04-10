3078e51bd224015c0b4fed912e6077886dae413b
eduardofloriani <eeduardofloriani@gmail.com>
2023-04-10 15:10:29 -0300

    initial structure for testing

---
+++ src/test/java/com/example/parking/controllers/ParkingControllerIT.java
@@ -0,0 +1,27 @@
+package com.example.parking.controllers;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class ParkingControllerIT {
+
+    @LocalServerPort
+    private int randomPort;
+
+    @BeforeEach
+    public void setUpTest() {
+        RestAssured.port = randomPort;
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void create() {
+    }
+}
